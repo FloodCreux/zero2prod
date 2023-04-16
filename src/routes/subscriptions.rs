a79407b8ba498e545c99e62c9141eef9bd961ef2
Mike Flood <mike@bodhi.solar>
2023-04-16 15:40:00 -0500

    chapters 1-3

---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,41 @@
+use actix_web::{web, HttpResponse};
+use sqlx::PgPool;
+use chrono::Utc;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String
+}
+
+pub async fn subscribe(
+    form: web::Form<FormData>,
+    pool: web::Data<PgPool>,
+) -> HttpResponse {
+    // `Result` has two variants; `Ok` and `Err`,
+    // The first for successes, the second for failures,
+    // We use a `match` statement to choose what to do based
+    // on the outcome.
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgPool`
+    // wrapped by `web::Data`.
+    .execute(pool.get_ref())
+    .await {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
+}
+
