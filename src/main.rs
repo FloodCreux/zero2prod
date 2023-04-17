6c74588c5d2e4a57d684c5d13885724e855de05b
Mike Flood <mike@bodhi.solar>
2023-04-16 21:20:29 -0500

    chapter 5: deployment

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 use std::net::TcpListener;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
@@ -13,13 +12,11 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
-    let connection_pool = PgPool::connect(
-            &configuration.database.connection_string().expose_secret()
-        )
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(configuration.database.with_db());
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
 
     let listener = TcpListener::bind(address)?;
 
